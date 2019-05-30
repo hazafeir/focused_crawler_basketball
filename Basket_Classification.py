fa26d24cf8f484238162df3407f8c2c287998701
hazafeir <hazafeir@uth.gr>
2019-05-30 16:30:51 +0300

    removed unecessary imports

--- Basket_Classification.py
+++ Basket_Classification.py
@@ -6,10 +6,8 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
 from collections import defaultdict
-from nltk.corpus import wordnet as wn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score
